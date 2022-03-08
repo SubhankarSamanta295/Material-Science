d72fcd3785f4ac3629e2f4549286d3afcf290960
SubhankarSamanta295 <76425638+SubhankarSamanta295@users.noreply.github.com>
2022-03-08 10:47:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from unicodedata import name
+from flask import Flask, render_template, request
+import model as m
+
+app = Flask(__name__)
+
+@app.route("/")
+def model():
+    if request.method == "POST":
+        formula = request.form["formula"]
+        #formula = [formula]
+        bulk = m.bulk_modulus_prediction(formula)
+        xyz = bulk
+
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    if request.method == "POST":
+        formula = request.form["formula"]
+        #formula = [formula]
+        bulk = m.bulk_modulus_prediction(formula)
+        xyz = bulk
+
+    return render_template("index.html", name = xyz)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
